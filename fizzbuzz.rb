e31db0de7d237c1cb2217127ba84f2c35ac16f5a
Abigail Mabeza <abigailmabeza@Abigails-MBP.home>
2016-03-29 03:56:59 -0400

    initial commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,25 @@
+
+
+num = 0 
+	while num <= 100 
+
+		if num%5 ==0 && num%3 ==0
+			puts "FizzBuzz"
+
+		elsif num%5 ==0
+			puts "Buzz"
+
+		elsif num%3 == 0
+			puts "Fizz"
+
+		else 
+				puts num
+		end
+
+			num+=1
+
+end
+
+
+
+
